9ccbe385e011f54e526ed65bb32b86d72e81d904
Your Name <you@example.com>
2018-03-23 15:52:32 +1100

    fix the logouot

--- src/store/http/httpAction.js
+++ src/store/http/httpAction.js
@@ -21,7 +21,8 @@ const dispatch = (request, dispatcher = () => {}) => {
   return request.then((data) => {
     if (data.status_code === UNAUTHENTICATION.CODE) {
       http('get', '/rest/V1/affiliate/logout', {}, { 'X-Requested-With': 'XMLHttpRequest' }, MagentoDomain).then((data) => {
-        if (data.success) {
+        const result = JSON.parse(data);
+        if (result.success) {
           window.location.href = getAbsolutePath(UNAUTHENTICATION.REWRIRE_URL, window.globalLanguage, { [UNAUTHENTICATION.REDIRECT_KEY]: window.location.href });
         }
       });
