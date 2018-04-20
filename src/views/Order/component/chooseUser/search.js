49f39316c3bcf8de19624ca3ddf61e7fdccd5af7
Your Name <you@example.com>
2018-04-20 16:35:48 +1000

    add the choose user

--- src/views/Order/component/chooseUser/search.js
+++ src/views/Order/component/chooseUser/search.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { intlShape, injectIntl } from 'react-intl';
-import { Button, Input } from 'antd';
+import { Button, Input, Icon } from 'antd';
 import classNames from 'classnames/bind';
 import styles from '../../Order.less';
 
@@ -14,20 +14,22 @@ const searchView = ({
   const { formatMessage } = intl;
   return (
     <div className={classNames(cx('choose-user-search'), 'block')}>
+      <div className={classNames(cx('tab-block-title'), 'block-title')} style={{paddingTop: 0}}>
+        <strong>搜索用户</strong>
+        <Button icon="close" className={cx('cancel-btn')} disabled={_.isEmpty(selectedUser)} onClick={() => { setSearchAreaVisible(false); }} />
+      </div>
       <div className="row">
         <div
-          className={classNames({
-            'col-md-12': _.isEmpty(selectedUser),
-            'col-md-10': !_.isEmpty(selectedUser),
-          })}
+          className="col-md-12"
         >
           <Search
             placeholder={formatMessage({ id: 'global.ui.input.searchUser' })}
             onSearch={value => searchByKeys(value)}
             enterButton
+            className={cx('search-input')}
           />
+
         </div>
-        { _.isEmpty(selectedUser) ? '' : <div className="col-md-2 text-right"><Button onClick={() => { setSearchAreaVisible(false); }}>{ formatMessage({ id: 'global.ui.button.cancel' }) }</Button></div> }
       </div>
 
     </div>
