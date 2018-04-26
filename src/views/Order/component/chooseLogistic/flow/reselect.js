c68d316bf76ed7e65d4ebb53603fc36b0848aa60
Your Name <you@example.com>
2018-04-26 16:30:40 +1000

    reselect the freight setting

---
+++ src/views/Order/component/chooseLogistic/flow/reselect.js
@@ -0,0 +1,21 @@
+import { createSelector } from 'reselect';
+import _ from 'lodash';
+
+const getAllFreightsettings = state => state.global.settings.freightSetting;
+const getOrderUser = state => state.global.orderUser;
+
+
+export const getAvailableFreightSettings = createSelector(
+  [
+    getAllFreightsettings,
+    getOrderUser,
+  ],
+  (allFreightSettings, orderUser) => {
+    debugger;
+    if (_.isEmpty(allFreightSettings) || _.isEmpty(orderUser && orderUser.country)) {
+      return [];
+    }
+    const { country } = orderUser;
+    return allFreightSettings.filter(f => f.allowed_countries.indexOf(country) > -1);
+  },
+);
