580d54e687afa3eccf1e8284eca6c3823f2fce57
Your Name <you@example.com>
2018-06-27 13:43:13 +1000

    fix the url bugs

--- src/components/page/HeaderContent/flow/reducer.js
+++ src/components/page/HeaderContent/flow/reducer.js
@@ -1,9 +1,8 @@
-import { combineReducers } from 'redux';
 import { LOCAL_STORAGE_CART_KEY } from 'config/magento.config';
 import { getStoreByKeys } from 'utils/localStorage';
 import { ADD_CART_COUNT } from './actionType';
 
-const count = (state = getStoreByKeys(LOCAL_STORAGE_CART_KEY) || 0, action) => {
+export const count = (state = getStoreByKeys(LOCAL_STORAGE_CART_KEY) || 0, action) => {
   switch (action.type) {
     case ADD_CART_COUNT:
       return state + 1;
@@ -11,6 +10,4 @@ const count = (state = getStoreByKeys(LOCAL_STORAGE_CART_KEY) || 0, action) => {
       return state;
   }
 };
-export default combineReducers({
-  count,
-});
+
