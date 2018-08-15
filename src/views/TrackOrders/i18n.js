88fc9775254a06cfc626815b3a7d39e395fadfc0
Your Name <you@example.com>
2018-08-15 11:44:04 +1000

    fix the fileter bug

--- src/views/TrackOrders/i18n.js
+++ src/views/TrackOrders/i18n.js
@@ -1,10 +1,10 @@
 export default {
   zh: {
     orderDetail: '订单详情',
-    deleteDeliverOrder: '确定删除该发货单？',
+    deleteDeliverOrder: '确定取消该发货单？',
   },
   en: {
     orderDetail: 'Order Detail',
-    deleteDeliverOrder: 'Are you sure to delete this delivery order？',
+    deleteDeliverOrder: 'Are you sure to cancel this delivery order？',
   },
 };
